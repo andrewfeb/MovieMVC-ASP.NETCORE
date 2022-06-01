237a101fd4fca2ea13a318a12b67b3eefb27a266
unknown <andr3w.febrianto@gmail.com>
2022-06-01 22:16:22 +0700

    add custom middleware

--- Startup.cs
+++ Startup.cs
@@ -38,12 +38,61 @@ namespace MovieMVC
 
             app.UseRouting();
 
+            // Example custom middleware using Run() method
+            /*app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Middleware 1");
+            });*/
+
+            // Example custom middleware using Use() method
+            /*app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Middleware 1: Incoming Request\n");
+                await next.Invoke();
+                await context.Response.WriteAsync("Middleware 1: Outgoing Response\n");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Middleware 2: Incoming Request\n");
+                await next.Invoke();
+                await context.Response.WriteAsync("Middleware 2: Outgoing Response\n");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Middleware 3: Incoming Request and response generated\n");
+            });*/
+
+            // Example custom middleware using Map() method
+            // access the url https://localhost:44369/movie and HandleMapMovie method will execute
+            app.Map("/movie", HandleMapMovie);
+
+            // access the url https://localhost:44369/contact and HandleMapContect method will execute
+            app.Map("/contact", HandleMapContact);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync(Configuration["ApplicationName"]);
                 });
+            });
+        }
+
+        private void HandleMapMovie(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Movie Page");
+            });
+        }
+
+        private void HandleMapContact(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Contact Page");
             });
         }
     }
